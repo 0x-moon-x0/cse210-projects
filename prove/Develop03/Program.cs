8839ab0f1a94cbb4a39b8132ffb4ae7af499520d
Kate <125932727+Xx-Moon-xX@users.noreply.github.com>
2024-02-07 13:18:54 -0700

    ---

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,7 +10,7 @@ class Program
         int numberToHide = random.Next(1, 5);
 
         Reference reference1 = new Reference("Proverbs", 3, 5, 6);
-        Scripture scripture1 = new Scripture(reference1, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. \nIn all thy ways acknowledge him, and he shall direct thy paths.");
+        Scripture scripture1 = new Scripture(reference1, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
 
         Reference reference2 = new Reference("John", 3, 16);
         Scripture scripture2 = new Scripture(reference2, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
