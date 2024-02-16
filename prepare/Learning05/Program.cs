b502652fee568650f9b0d7555b7956e005c57c42
Kate <125932727+Xx-Moon-xX@users.noreply.github.com>
2024-02-16 00:43:35 -0700

    Completed Learning Activity W05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 15);
+        // Console.WriteLine($"The square is {square.GetColor()} with an area of {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("red", 10, 3);
+        // Console.WriteLine($"The rectangle is {rectangle.GetColor()} with an area of {rectangle.GetArea()}");
+
+        Circle circle = new Circle("yellow", 13);
+        // Console.WriteLine($"The circle is {circle.GetColor()} with an area of {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape> 
+        {
+            square, rectangle, circle
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
