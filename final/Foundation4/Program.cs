56d4ed847089b1cbe05ff313fd217b5216b4e3dd
Kate <125932727+Xx-Moon-xX@users.noreply.github.com>
2024-02-21 21:00:59 -0700

    idk...

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,13 +6,13 @@ class Program
     {
         List<Activity> activities = new List<Activity>();
 
-        Running running = new Running("22 May 2024", 45, 20);
+        Running running = new Running("22 May 2024", 45, 5);
         activities.Add(running);
 
         Cycling cycling = new Cycling("15 Mar 2024", 30, 15);
         activities.Add(cycling);
 
-        Swimming swimming = new Swimming("01 Jul 2024", 60, 10);
+        Swimming swimming = new Swimming("01 Jul 2024", 60, 40);
         activities.Add(swimming);
 
         foreach (Activity activity in activities)
